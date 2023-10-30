aed58aaeedb9905e195aa1c65c2e03ca6f9f0846
Tamás Vince Kornél <vin@kys.social>
2023-10-30 21:25:21 +0100

    meg fogom olni magam

--- tizedik/feladatsor/05/alap/main.py
+++ tizedik/feladatsor/05/alap/main.py
@@ -1,7 +1,10 @@
-def take_int(msg, range_start=None, range_end=None):
+def take_int(range_start=None, range_end=None, msg="Kérek egy számot"):
   while True:
     try:
-            
+      if not range_start == None and not range_end == None:
+        msg += f" ({range_start}-{range_end}): "
+      else:
+        msg += ": "
       num = int(input(msg))
       if range_start == None and range_end == None:
         return num
@@ -13,13 +16,13 @@ def take_int(msg, range_start=None, range_end=None):
       print("A megadott érték nem szám!")
 
 # 1. feladat
-a = take_int("Kérek egy számot 1-10: ", 1, 10)
+a = take_int(1, 10)
 
 for i in range(1, a+1):
   print(f"{i}. Egy fecske nem csinál nyarat.")
 
 # 2. feladat
-a = take_int("Kérek egy számot 1-10: ", 1, 10)
+a = take_int(1, 10)
 
 for i in range(1, a+1):
   print(f"Megmondtam már {i}-szer, hogy semmit nem mondok el kétszer.")
@@ -62,7 +65,7 @@ for i in range(1000, 0, -3):
 for i in range(100):
   print("*", end="")
 
-a = take_int("Kérek egy számot: ")
+a = take_int()
 b = input("Kérek egy karaktert: ")
 
 for i in range(a):
@@ -81,37 +84,37 @@ for i in range(8):
 
 
 # 5. feladat
-a = take_int("Kérek egy számot: ")
-b = take_int("Kérek egy számot: ")
-c = take_int("Kérem a lépésközt: ")
+a = take_int()
+b = take_int()
+c = take_int(msg="Kérem a lépésközt: ")
 
 for i in range(a, b+1, c):
   print(i, end=" ")
 
 
 # 6. feladat
-a = take_int("Kérek egy számot: ")
+a = take_int()
 
 for i in range(1, a+1):
   print(i**2, end="; ")
 
 
 # 7. feladat
-a = take_int("Kérek egy számot: ")
+a = take_int()
 
 for i in range(1, a+1):
   print(i**3)
   
 
 # 8. feladat
-a = take_int("Kérek egy számot: ")
+a = take_int()
 
 for i in range(1, a+1):
   print(f"{i} {i**0.5:.2f}")
 
 
 # 9. feladat
-a = take_int("Kérek egy számot: ")
+a = take_int()
 
 f = 1
 for i in range(1, a+1):
@@ -120,14 +123,14 @@ print(f)
 
 
 # 10. feladat
-a = take_int("Kérek egy számot: ")
+a = take_int()
 
 for i in range(1, a+1):
   print(i**2)
 
 
 # 11. feladat
-a = take_int("Kérek egy számot: ")
+a = take_int()
 t = 0
 for i in range(0, a):
   if i % 2:
@@ -136,7 +139,7 @@ print(t)
 
 
 # 12. feladat
-a = take_int("Kérek egy számot: ")
+a = take_int()
 
 t = 0
 for i in range(1, a+1):
@@ -144,14 +147,14 @@ for i in range(1, a+1):
 
 
 # 13. feladat
-a = take_int("Kérek egy számot: ")
+a = take_int()
 
 for i in range(1, a+1):
   print(i*3)
 
 
 # 14. feladat
-n = take_int("Kérek egy számot: ")
+n = take_int()
 
 def fib(n):
   if n <= 1:
@@ -164,7 +167,7 @@ for i in range(n):
 
 
 # 15. feladat
-n = take_int("Kérek egy számot: ")
+n = take_int()
 
 def is_prime(n):
   if n <= 1:
@@ -187,13 +190,142 @@ for i in range(1, 11):
 
 
 # 17. feladat
+from random import randint
 def rand(range_start, range_end):
-  from random import randint
   return randint(range_start, range_end)
 
 ranges = [[0,10], [0,25], [0,50], [10,75], [-50,50], [-100,-70]]
 for x in ranges:
   print(f"{rand(x[0], x[1])} ", end="")
 
-
 # 18. feladat
+a = take_int()
+
+# 1 sor
+print("*"*a)
+
+# négyzet
+for i in range(a):
+  print("*"*a)
+
+
+# 19. feladat
+a = take_int()
+b = take_int()
+
+for i in range(a):
+  print("*"*b)
+
+
+# 20. feladat
+a = take_int()
+b = take_int()
+
+for i in range(a):
+  print(" "*(a-i), "*"*b)
+
+
+# 21. feladat
+a = take_int()
+
+for i in range(a):
+  print(" "*(a-i) + "*"*(i*2+1))
+
+
+# 22. feladat
+a = take_int()
+b = take_int()
+
+for i in range(a):
+  if i == 0 or i == a-1:
+    print("*"*b)
+  else:
+    print("*" + " "*(b-2) + "*")
+
+
+# 23. feladat
+from random import randint
+def rand(range_start, range_end):
+  return randint(range_start, range_end)
+
+a = take_int()
+
+good = 0
+bad = 0
+
+for i in range(a):
+  x = rand(1, 100)
+  y = rand(1, 100)
+  print(f"{x} + {y} = ", end="")
+  z = take_int()
+  if z == x+y:
+    print("Helyes!")
+    good += 1
+  else:
+    print("Helytelen!")
+    bad += 1
+  
+  print(f"{x} - {y} = ", end="")
+
+  z = take_int()
+  if z == x-y:
+    print("Helyes!")
+    good += 1
+  else:
+    print("Helytelen!")
+    bad += 1
+
+print(f"Helyes: {good}, Helytelen: {bad}")
+print(f"Arány: {good/(good+bad)*100:.2f}%")
+
+# 24. feladat
+for i in range(32, 256):
+  print(f"{i:3} {chr(i)}")
+
+
+# 25. feladat
+a = take_int()
+
+for i in range(1, a+1):
+  if a % i == 0:
+    print(i, end=" ")
+
+
+# 26. feladat
+a = take_int()
+
+s = 0
+for i in range(1, a+1):
+  if a % i == 0:
+    s += i
+
+print(s)
+
+
+# 27. feladat
+a = take_int()
+
+s = 0
+for i in range(1, a):
+  if a % i == 0:
+    s += i
+
+if s == a:
+  print("Tökéletes szám!")
+else:
+  print("Nem tökéletes szám!")
+
+
+# 28. feladat
+a = take_int(msg="Kérem a hatványalapot: ")
+b = take_int(msg="Kérem a kitevőt: ")
+
+print(a**b)
+
+
+# 29. feladat
+for i in range(65, 91):
+  for j in range(65, 91):
+    print(chr((i+j-130)%26+65), end="")
+  print()
+
